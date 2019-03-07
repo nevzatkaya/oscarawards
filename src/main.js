8399f83809204259342fbf513bf48ea3d58430c9
nevzatkaya <nevzatkaya@gmail.com>
2019-03-07 22:41:36 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+
+
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'popper.js/dist/popper.js'
+
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App)
